f2a1309c99e4e0d1c13cd1f7dffc10a53f287596
dudasim1990 <dudasim1990@yandex.com>
2021-07-08 14:56:11 +0500

    Added files

---
+++ sctipt.js
@@ -0,0 +1,9 @@
+let name = prompt(" Ismingizni kiriting ");
+let surname = prompt(" Familiyangizni kiriting ");
+let profession = prompt(" Kasbingizni kiriting ");
+let age = prompt(" Tugilgan sanangizni kiriting ");
+let gender = prompt(" Jinsingiz ");
+let number = prompt (" Raqamingizni kiriting ")
+let email = prompt (" Emailingizmi kiriting ")
+
+alert ("Sizning ismingiz  "  +  " "  +  name  +  "\nFamiliyangiz   "  +  surname  +  " \nSizning kasbingiz   "  +  profession  +  "\nTugilgan sanangiz   "  +  age  +  " "  +  "\nJinsingiz   "  +  gender  +  " "  +  "\nSizning raqamingiz   " + number + " " + "\nSizning emailingiz   " + email )
